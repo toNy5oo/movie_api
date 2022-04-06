fcec35d152e516faa04ffb1bfa2ae93d63e72d94
malandr1n0 <lindirizzoemail@gmail.com>
2022-04-06 15:12:54 +0200

    Written documentation | Updated URL Endpoints with Logic

--- index.js
+++ index.js
@@ -12,59 +12,111 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { 
 
 let dataJSON = [{
         title: 'Pulp Fiction',
-        director: 'Quentin Tarantino'
+        director: 'Quentin Tarantino',
+        genre: 'Splatter'
     },
     {
         title: 'Lord of the Rings',
-        director: 'Peter Jackson'
+        director: 'Peter Jackson',
+        genre: 'Fantasy'
     },
     {
         title: 'Matrix',
-        director: 'The Wachowskis'
+        director: 'The Wachowskis',
+        genre: 'Action'
     },
     {
         title: 'Life is beautiful',
-        director: 'Roberto Benigni'
+        director: 'Roberto Benigni',
+        genre: 'Drama/Historic'
     },
     {
         title: 'Fight Club',
-        director: 'David Fincher'
+        director: 'David Fincher',
+        genre: ''
     },
     {
         title: 'Forrest Gump',
-        director: 'Robert Zemeckis'
+        director: 'Robert Zemeckis',
+        genre: ''
     },
     {
         title: 'The Warriors',
-        director: 'Walter Hill'
+        director: 'Walter Hill',
+        genre: ''
     },
     {
         title: '12 years a slave',
-        director: 'Steve McQueen'
+        director: 'Steve McQueen',
+        genre: ''
     },
     {
         title: 'The Shawshank Redemption',
-        director: 'Frank Darabont'
+        director: 'Frank Darabont',
+        genre: ''
     },
     {
         title: 'The Godfather',
-        director: 'Francis Ford Coppola'
+        director: 'Francis Ford Coppola',
+        genre: ''
     }
 ];
 
 app.use(morgan('combined', { stream: accessLogStream }));
 
+//Return a list of ALL movies to the user
 app.get('/movies', (req, res) => {
     res.json(dataJSON);
 });
 
-// app.get('/documentation', (req, res) => {
-//     res.sendFile('public/documentation.html', { root: __dirname });
-// });
+//Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user
+app.get('/movies/:title', (req, res) => {
+    //res.send('Looking for movie ' + req.params.title);
+    res.json(dataJSON.find((movie) => { return movie.title === req.params.title; }));
+});
 
-//Version with express
-app.get('/', (req, res) => {
-    res.send('Welcome to my app');
+//Return data about a genre (description) by name/title (e.g., “Thriller”)
+app.get('/movies/:title/details', (req, res) => {
+
+    let movie = dataJSON.find((movie) => { return movie.title === req.params.title; });
+
+    if (movie) {
+        console.log('Movie found: ' + movie.title);
+        res.send(movie.genre);
+    } else
+        res.send('Movie not found');
+});
+
+//Return data about a director (bio, birth year, death year) by name
+app.get('/movies/directors/:name', (req, res) => {
+    res.send('Details about the director: ' + req.params.name);
+});
+
+//Allow new users to register
+app.post('/users/register', (req, res) => {
+    res.status(201);
+    res.send('User account created');
+});
+
+//Allow users to update their user info (username)
+app.put('/users/:user', (req, res) => {
+    res.send('User account updated');
+});
+
+//Allow users to add a movie to their list of favorites (showing only a text that a movie has been added—more on this later)
+app.post('/users/:user/favourites', (req, res) => {
+    res.status(201);
+    res.send('The movie has been add to favorites');
+});
+
+//Allow users to remove a movie from their list of favorites (showing only a text that a movie has been removed—more on this later)
+app.delete('/users/:user/favourites', (req, res) => {
+    res.send('The movie has been removed from favorites');
+});
+
+//Allow existing users to deregister (showing only a text that a user email has been removed—more on this later)
+app.delete('/users/:user', (req, res) => {
+    res.send('You have been removed')
 });
 
 app.use(express.static('public'));
