5e9526abc45e3d5adb9b4291b4594497ccaa7b14
malandr1n0 <lindirizzoemail@gmail.com>
2022-05-11 09:56:43 +0200

    Auth to get movies list disabled

--- routes/movies.js
+++ routes/movies.js
@@ -11,8 +11,9 @@ const Directors = Models.Director;
 
 require('../passport');
 
+//passport.authenticate('jwt', { session: false }),
 //Return a list of ALL movies to the user
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get('/', (req, res) => {
     Movies.find()
         .then((movies) => res.json(movies))
         .catch((err) => {
