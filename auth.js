b522ddde352e50c86d6341bc3fa82e7e78a79969
malandr1n0 <lindirizzoemail@gmail.com>
2022-05-16 19:10:08 +0200

    Reverted commit

--- auth.js
+++ auth.js
@@ -17,7 +17,7 @@ module.exports = (router) => {
         passport.authenticate('local', { session: false }, (error, user, info) => {
             if (error || !user) {
                 return res.status(400).json({
-                    message: 'Something is wrong',
+                    message: 'Something is wrong ' + error.message,
                     user: user
                 });
             }
