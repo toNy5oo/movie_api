e855f92baa07826638df27b30b5a54dfae748070
malandr1n0 <lindirizzoemail@gmail.com>
2022-07-14 12:17:59 +0200

    Docs from JSDOC added

--- README.md
+++ README.md
@@ -12,6 +12,45 @@ Express(.js) - Node.js web framework
 React(.js) - a client-side JavaScript framework
 Node(.js) - the premier JavaScript web server
 
-<h3>Links</h3>
+<h3>Requirements</h3>
+The API must be a Node.js and Express application.
+The API must use REST architecture, with URL endpoints corresponding to the data operations listed above
+The API must use at least three middleware modules, such as the body-parser package for reading data from requests and morgan for logging.
+The API must use a “package.json” file.
+The database must be built using MongoDB.
+The business logic must be modeled with Mongoose.
+The API must provide movie information in JSON format.
+The JavaScript code must be error-free.
+The API must be tested in Postman.
+The API must include user authentication and authorization code.
+The API must include data validation logic.
+The API must meet data security regulations.
+The API source code must be deployed to a publicly accessible platform like GitHub.
+The API must be deployed to Heroku.
 
+<h3>Dependencies:</h3>
+"bcrypt": "^5.0.1",
+"body-parser": "^1.19.1",
+"cors": "^2.8.5",
+"express": "^4.17.2",
+"express-validator": "^6.14.0",
+"jsonwebtoken": "^8.5.1",
+"lodash": "^4.17.21",
+"mongoose": "^6.2.0",
+"morgan": "^1.10.0",
+"passport": "^0.5.2",
+"passport-jwt": "^4.0.0",
+"passport-local": "^1.0.0",
+"serve": "^13.0.2",
+"uuid": "^8.3.2"
+devDependencies
+"eslint": "^8.7.0",
+"nodemon": "^2.0.15"
 
+<h3>Tech:</h3>
+Node
+Express
+MongoDB
+Mongoose
+MongoDB Compass
+Heroku
--- routes/genres.js
+++ routes/genres.js
@@ -1,31 +1,53 @@
-const router = require('express').Router();
-const Models = require('../models.js');
-const passport = require('passport')
+const router = require("express").Router();
+const Models = require("../models.js");
+const passport = require("passport");
 
 //Defining Documents variables from Database
 const Genres = Models.Genre;
 
-require('../passport');
+require("../passport");
 
-//passport.authenticate('jwt', { session: false }),
-//Return a list of ALL genres
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
-    Genres.find()
-        .then((genres) => res.json(genres))
-        .catch((err) => {
-            console.error(err);
-        });
-});
+/**
+ * GET: Returns a list of ALL genres
+ * @function [path]/genres/
+ * @returns {Object[]} genres
+ * @requires passport
+ */
+router.get(
+    "/",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Genres.find()
+            .then((genres) => res.json(genres))
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+);
 
-//Return data (name, description) about a genre by id
-router.get('/:genre_id', passport.authenticate('jwt', { session: false }), (req, res) => {
-    Genres.findById(req.params.genre_id)
-        .then(genres => {
-            //Checking if the returned Object isn't empty
-            if (Object.keys(genres).length != 0) res.json(genres)
-            else { res.status(400).send(req.params._id + ' is not a valid id.') }
-        })
-        .catch((err) => res.status(500));
-});
+/**
+ * GET: Returns data about a single genre by id
+ * @function [path]/genres/:genre_id
+ * @param {any} genre_id
+ * @returns {Object} genre
+ * @requires passport
+ */
+router.get(
+    "/:genre_id",
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+        Genres.findById(req.params.genre_id)
+            .then((genres) => {
+                //Checking if the returned Object isn't empty
+                if (Object.keys(genres).length != 0) res.json(genres);
+                else {
+                    res.status(400).send(
+                        req.params._id + " is not a valid id."
+                    );
+                }
+            })
+            .catch((err) => res.status(500));
+    }
+);
 
 module.exports = router;
